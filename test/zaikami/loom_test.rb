57a034067b695795e68ead1e8d881112feea03ce
Martin Spickermann <spickermann@gmail.com>
2019-12-09 13:50:10 +0100

    Switch from RSpec to Minitest

---
+++ test/zaikami/loom_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+class Zaikami::LoomTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Zaikami::Loom::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
